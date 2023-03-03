64cab2be28eb2606735e91016543aaa65d58d841
Randal Sean Harrison <rharriso@nd.edu>
2023-03-03 15:21:46 -0500

    updated the dice page

--- js/die-roller.js
+++ js/die-roller.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-
-  $(document).on("click", "#die-roller .dicey", function() {
-
+$(document).ready(function () {
+  $(document).on("click", "#die-roller .dicey", function () {
     // Get the die clicked on and create random number
     var dieClickedOn = $(this).data("die");
-    var roll = (Math.floor(Math.random() * dieClickedOn)) + 1;
+    var roll = Math.floor(Math.random() * dieClickedOn) + 1;
     // Shake the die
-    $(this).effect("shake", {
-      "distance": 3,
-      "times": 2
-    });
+
+    if ($(this).hasClass("bg-green") || $(this).hasClass("bg-blue")) {
+      $(this).fadeOut(100).fadeIn(100);
+    } else {
+      $(this).fadeOut(100).fadeIn(100);
+    }
 
     // Fade the die roll results circle
     $("#die-roll-result span").stop().fadeOut(150).fadeIn(150);
@@ -21,7 +21,6 @@ $(document).ready(function() {
     // fade all others but die clicked on
     $(".dicey").addClass("reduced-opacity");
     $(this).removeClass("reduced-opacity");
-
 
     // Set the roll color and die wobble
     switch (dieClickedOn) {
@@ -40,11 +39,7 @@ $(document).ready(function() {
       case 10:
         $("#die-roll-result span").removeClass("reduced-opacity employee-green unemployed-red manager-purple").addClass("owner-blue");
         break;
-
     }
-
   });
-
-
 });
 // document ready closes
