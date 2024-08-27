b0afc722e6feea676c325a07c0c713c5b6bd18bd
Randal Sean Harrison <rharriso@nd.edu>
2024-08-27 13:43:36 -0400

    replaced header with dynamic load

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-  // Load footer dyamically
+  // Load footer and header dyamically
   $("body #footer").load("footer.html", function () {
     console.log("Footer loaded");
     $('[data-toggle="tooltip"]').tooltip();
+
+    $("header").load("header.html", function () {
+      console.log("header loaded!");
+    });
+
   });
-  $("header").load("header.html", function () {
-    console.log("header loaded!");
-  });
+
 });
 // document.ready
