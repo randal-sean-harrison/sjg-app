a65ec19a3c281a4a19fc72dcecba11116fd8857d
Randal Sean Harrison <rharriso@nd.edu>
2024-08-27 13:37:52 -0400

    changed the interface to buttons

--- js/main.js
+++ js/main.js
@@ -4,5 +4,9 @@ $(document).ready(function () {
     console.log("Footer loaded, dooood!");
     $('[data-toggle="tooltip"]').tooltip();
   });
+  $("header").load("header.html", function () {
+    console.log("header loaded, dooood!");
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 });
 // document.ready
